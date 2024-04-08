592870a15994bf641ee6339e557467f1cb555173
OksanaTryhub <ksanatrigub@gmail.com>
2024-04-08 12:55:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className='flex bg-[#EABD4D] w-full py-10 px-5 gap-2 justify-between'>
+      <h1 className='text-[#473A3F] font-bold text-2xl'>Regal Estate</h1>
+      <p>Welcome to property management app</p>
+    </div>
+  );
+};
+
+export default App;
