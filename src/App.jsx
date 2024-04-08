2629345a8fdd3b141f7305e0632027b9913aadc6
OksanaTryhub <ksanatrigub@gmail.com>
2024-04-08 13:11:02 +0300

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 const App = () => {
   return (
-    <div className='flex bg-[#EABD4D] w-full py-10 px-5 gap-2 justify-between'>
-      <h1 className='text-[#473A3F] font-bold text-2xl'>Regal Estate</h1>
-      <p>Welcome to property management app</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
